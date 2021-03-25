5f9d677d0433922a5555494764525765473e8cce
Mallikarjun Gunda <mallikarjungunda@gmail.com>
2021-03-25 10:17:50 -0400

    Add files via upload

---
+++ src/test/java/com/example/swarnaproject/SwarnaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swarnaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarnaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
